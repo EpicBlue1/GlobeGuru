5de2432780975a62d4926ec389f7f52ef73f6d9e
EpicBlue1 <21100115@virtualwindow.co.za>
2023-05-31 21:14:53 +0200

    Different Size Screens

--- app/src/main/java/com/example/globeguru/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/globeguru/screens/RegisterScreen.kt
@@ -14,8 +14,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -77,6 +79,7 @@ fun RegisterScreen(
 
     Column(modifier = Modifier
         .fillMaxSize()
+        .verticalScroll(rememberScrollState())
         .background(appDarkGray), horizontalAlignment = Alignment.CenterHorizontally) {
         Column(modifier = Modifier
             .fillMaxWidth()
